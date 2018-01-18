3cd4c714f117435145fa55fa55d2cd0e1eed187b
Sahil Thakur <sahilthakur@Sahils-MacBook-Pro.local>
2018-01-19 00:17:28 +0530

    added my naive solution

--- 915C_permuteDIgits.py
+++ 915C_permuteDIgits.py
@@ -11,3 +11,30 @@ while(len(a)>0):
             a=a[:i]+a[i+1:]
             break
 print(fix)
+
+'''
+#my first solution - gave memory exceeded error, where i was calulating every permute and storing
+
+import itertools
+
+a = raw_input()
+b = raw_input()
+
+a = list(a)
+b = int(b)
+
+p_list = list(itertools.permutations(a,len(a)))
+#p_list = [[int(x) for x in y] for y in p_list]
+
+ans = 0
+a_ans = ''
+for i in p_list:
+    tsum = ''
+    for j in range(len(a)):
+        tsum += i[j]
+    if int(tsum) <= b and int(tsum) > ans:
+        ans = int(tsum)
+        a_ans = tsum
+
+print a_ans 
+'''
